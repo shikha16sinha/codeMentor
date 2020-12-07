7a5d2b13c90dbb2dbdf95a4e868c51d9ee1c80ce
shikha16sinha <61140393+shikha16sinha@users.noreply.github.com>
2020-12-08 01:45:52 +0530

    Update rssfeed.py

--- rssfeed.py
+++ rssfeed.py
@@ -1,9 +1,8 @@
 import sys
 import feedparser
 
-#NewsFeed = feedparser.parse(str(sys.argv[1]))
 NewsFeed = feedparser.parse("https://timesofindia.indiatimes.com/rssfeedstopstories.cms")
-print NewsFeed
+
 descp = NewsFeed.feed.subtitle
 entry=NewsFeed.feed.title_detail.value
 link = NewsFeed.feed.links[0].href
